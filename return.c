fd7e2faf42e387d2e47bdd5b4aa7e02b6b35e002
RahulSlays18 <rahulpandeyoriginal18@gmail.com>
2024-01-07 19:00:53 +0530

    first commit

--- return.c
+++ return.c
@@ -3,6 +3,6 @@ int main(void)
 {
     char your_name[100];
     printf("Enter your name:");
-    scanf(" %[^\n]",&your_name);
+    scanf(" %s[^\n]",&your_name);
     printf("Hello %s\n",your_name);
 }
